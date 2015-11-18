a4a480aaea7a02a1aa80811e7b3d69ce8cdc71ca
上海－郭朝阳 <guochaoyang@jd.com>
2015-11-18 19:17:18 +0800

    1

---
+++ SpriteKit学习-02/SailorMoon.h
@@ -0,0 +1,13 @@
+//
+//  SailorMoon.h
+//  SpriteKit学习-02
+//
+//  Created by guochaoyang on 15/11/18.
+//  Copyright © 2015年 guochaoyang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SailorMoon : SKScene
+
+@end
---
+++ SpriteKit学习-02/SailorMoon.m
@@ -0,0 +1,18 @@
+//
+//  SailorMoon.m
+//  SpriteKit学习-02
+//
+//  Created by guochaoyang on 15/11/18.
+//  Copyright © 2015年 guochaoyang. All rights reserved.
+//
+
+#import "SailorMoon.h"
+
+@implementation SailorMoon
+
+- (void)didMoveToView:(SKView *)view
+{
+    SKSpriteNode *moon = [SKSpriteNode spriteNodeWithImageNamed:];
+}
+
+@end
